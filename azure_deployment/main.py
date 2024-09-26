f875ad20d884f710101006e2018989b59c6a9932
Kevin SIMON <simonkvin@gmail.com>
2024-09-26 11:41:34 +0200

    Ajouter les variables d'environnement pour le nom d'utilisateur et le mot de passe locaux

--- azure_deployment/main.py
+++ azure_deployment/main.py
@@ -1,22 +1,51 @@
+import os
+from tkinter.font import names
 import streamlit as st
+import streamlit_authenticator as stauth
 from styles import apply_custom_styles
-from pages import chatbot
+from pages.chatbot import display_accueil
 
+# Set page configuration
+st.set_page_config(layout="wide")
+apply_custom_styles()
 
 def main():
-    """
-    Main function of the application.
+    names = ["Kevin"]
+    usernames = [os.getenv("RAG_LOCAL_USERNAME")]
+    passwords = [os.getenv("RAG_LOCAL_PASSWORD")]
+    hashed_passwords = stauth.Hasher(passwords).generate()
 
-    :return: None
-    """
+    # Create credentials dictionary
+    credentials = {
+        "usernames": {
+            usernames[i]: {"name": names[i], "password": hashed_passwords[i]}
+            for i in range(len(usernames))
+        }
+    }
 
-    st.title("Profiles Finder")
-    try:
-        chatbot.display_accueil()
-        apply_custom_styles()
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    # Create an authenticator object
+    authenticator = stauth.Authenticate(
+        credentials, "some_cookie_name", "some_signature_key", cookie_expiry_days=10
+    )
+
+    st.title("PROFILE FINDER")
+    # Render login widget
+    name, authentication_status, username = authenticator.login(
+        "main", None, 3, None, False, False, "Login"
+    )
+
+    if authentication_status:
+        st.sidebar.write(f"Bienvenue *{name}*")
+        authenticator.logout("Logout", "sidebar")
+
+        display_accueil()
+
+    elif authentication_status == False:
+        st.error("Username/password is incorrect")
+    elif authentication_status == None:
+        st.warning("Please enter your username and password")
 
 
 if __name__ == "__main__":
     main()
+
